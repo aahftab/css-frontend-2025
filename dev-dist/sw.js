4c9858551965d3ed028520e388eff57e28a5e458
Aftab Ansari <aahftab@gmail.com>
2025-02-11 02:22:10 +0530

    dev-dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7an4dlehr2g"
+    "revision": "0.70frcpti908"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
